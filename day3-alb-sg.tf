665e07ca2a2fb108f18f72c41cf159196db3571d
gopal1409 <gopal1409@gmail.com>
2026-09-09 15:17:30 +0530

    Create day3-alb-sg.tf

---
+++ day3-alb-sg.tf
@@ -0,0 +1,39 @@
+#create a new file alb-sg.tf copy line 2 till 36
+resource "aws_security_group" "alb_sg" {
+  name        = "${local.project_tags.Name}-alb-sg"
+  description = "Security group for web servers"
+  vpc_id      = aws_vpc.my_vpc.id #this sg will be attached with your vpc
+  # ... other configuration ...
+
+  ingress  {
+    description = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress  {
+    description = "HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  egress {
+    description = "allow all outbound rule"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+tags = {
+    Name = "${local.project_tags.Name}-alb-sg"
+   
+  }
+}
+##after this do terraform apply
+#lets create the sg
+  ##once created go to security group in ec2 you will see albsg only open port 443 and 80
